cc90cde90a3107437ffca86171b17f2a37746657
kibrom01-star <kbromfisha2@gmail.com>
2023-06-16 09:01:18 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{        
-int i;        
+{       
+int i;
 for (i = 0; i < 10; i++)
-{                
-printf("%d", i);        
-}        
-putchar('\n');        
+{
+printf("%d", i);
+}
+putchar('\n');
 return (0);
 }
