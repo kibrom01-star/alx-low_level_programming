c65bcdb4cff45470c714121a643c43f3d5973721
kibrom01-star <kbromfisha2@gmail.com>
2023-06-16 09:07:41 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{        
-char ch;        
-for (ch = 'z'; ch >= 'a'; ch--)        
-{                
-putchar(ch);       
-}        
-putchar('\n');        
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
 return (0);
 }
