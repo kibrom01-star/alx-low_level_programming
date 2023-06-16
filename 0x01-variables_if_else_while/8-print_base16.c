a747a171df5cb5732a9bfaf470dec73d016ab5c1
kibrom01-star <kbromfisha2@gmail.com>
2023-06-16 09:09:58 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,16 +5,16 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
-int i;        
-for (i = 48; i < 58; i++)        
-{                
-putchar(i);        
-}        
-for (i = 97; i < 103; i++)        
-{                
-putchar(i);        
-}        
-putchar('\n');        
+{
+int i;
+for (i = 48; i < 58; i++)
+}
+putchar(i);
+}
+for (i = 97; i < 103; i++)
+{
+putchar(i);
+}
+putchar('\n');
 return (0);
 }
