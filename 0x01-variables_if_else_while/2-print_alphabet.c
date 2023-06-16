ca520e4d9e0fb3ec4fbb0740bc464a76211055ee
kibrom01-star <kbromfisha2@gmail.com>
2023-06-16 08:56:02 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
-char alp[26] = "abcdefghijklmnopqrstuvwxyz";        
-int i;       
-for (i = 0; i < 26; i++)        
-{        
-putchar(alp[i]);        
-}       
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
 putchar('\n');
 return (0);
 }
